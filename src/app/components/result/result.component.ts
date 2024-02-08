770a4bbc046729db6194830ff4607b23b32fffb6
zayar <minzayarmaung2002@gmail.com>
2024-02-08 17:26:59 +0630

    get error need to be fixed

--- src/app/components/result/result.component.ts
+++ src/app/components/result/result.component.ts
@@ -1,19 +1,31 @@
-import { Component } from '@angular/core';
+// result.component.ts
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { DataService } from 'src/app/data.service';
+
+interface User {
+  // Define properties based on the structure of the user data
+  id: number;
+  name: string;
+  // Add more properties as needed
+}
 
 @Component({
   selector: 'app-result',
   templateUrl: './result.component.html',
   styleUrls: ['./result.component.css']
 })
-export class ResultComponent {
-  users : any[] = []
+export class ResultComponent implements OnInit {
+  users: User[] | undefined;
+  url: string = "http://localhost:8080/";
+
+  constructor(private service: DataService, private router: Router) {}
 
   ngOnInit(): void {
-    this.users = [
-        
-    ];
-    
+    this.service.getResultTable().subscribe((data: User[]) => {
+      this.users = data;
+    });
   }
 
-  filterChange:string = "" // Responsible for Searching Data and Flitering Data
+  filterChange: string = ""; // Responsible for Searching Data and Filtering Data
 }
